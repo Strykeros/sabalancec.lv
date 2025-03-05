a3ee068a514b66921ad0038ae6424346ade2efb8
Rainers K. <rainerkrum@gmail.com>
2025-03-05 19:04:17 +0200

    updated home page

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,6 +1,7 @@
 <header class="bg-gray-800 p-4">
 	<nav class="container mx-auto">
 		<ul class="flex justify-center space-x-4">
+			<li><a href="/" class="text-white hover:text-gray-400">Home</a></li>
 			<li><a href="/shop" class="text-white hover:text-gray-400">Shop</a></li>
 			<li><a href="/allergens" class="text-white hover:text-gray-400">Allergens</a></li>
 			<li><a href="/account" class="text-white hover:text-gray-400">Account</a></li>
