8033748f8a46ea207fdc54796613adc8fcbd26c9
Rainers K. <rainerkrum@gmail.com>
2025-03-06 19:10:07 +0200

    setup .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
 
+dotenv.config();
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [sveltekit(), tailwindcss()],
+	server: {
+		port: parseInt(process.env.PORT || '3000')
+	}
 });
